ea7216192901ccc279a1df840983315d36de3898
ealborn <emma.alborn@gmail.com>
2018-03-22 15:02:07 +0100

    initial commit
    starting up repo

---
+++ ArtShop/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+
+namespace ArtShop.Controllers
+{
+    //using ArtShop.Models;
+
+    public class ProductsController : Controller
+    {
+        private string connectionString;
+
+        public IActionResult Index()
+        {
+            //return View(new List<ProductViewModel>());//ersätt med resultat från databasen
+
+            List<ProductViewModel> news;
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                news = connection.Query<ProductViewModel>("select * from Paintings").ToList();
+            }
+
+            return View(news);
+
+
+
+        }
+    }
+}
---
+++ ArtShop/Models/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtShop.Models;
+
+namespace ArtShop.Models
+{
+    public class ProductViewModel
+    {
+        //get och set properties
+        //från databasens kolumner
+        public string Id { get; set; }
+        public string Title {get; set;}
+        public string Description {get; set;}
+        public string Technique {get; set;}
+        public string Image {get; set;}
+        public string Price { get; set; }
+    }
+}
